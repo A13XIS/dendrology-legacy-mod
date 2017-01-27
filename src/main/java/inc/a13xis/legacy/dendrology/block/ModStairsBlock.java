32d38c43ec911bb339aefe2853023653d39ba82c
BuildTools <unconfigured@null.spigotmc.org>
2017-01-27 23:27:17 +0100

    Fix Stairs logic and a block model bug

--- src/main/java/inc/a13xis/legacy/dendrology/block/ModStairsBlock.java
+++ src/main/java/inc/a13xis/legacy/dendrology/block/ModStairsBlock.java
@@ -3,9 +3,7 @@ package inc.a13xis.legacy.dendrology.block;
 import inc.a13xis.legacy.dendrology.TheMod;
 import inc.a13xis.legacy.koresample.common.block.StairsBlock;
 import inc.a13xis.legacy.koresample.tree.DefinesStairs;
-import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
@@ -33,13 +31,50 @@ public final class ModStairsBlock extends StairsBlock
 
     @Override
     public IBlockState getStateFromMeta(int meta) {
-        return getDefaultState();
+        IBlockState state = getDefaultState();
+        if((meta/2)==0){
+            state = state.withProperty(HALF,EnumHalf.BOTTOM);
+        }
+        else{
+            state = state.withProperty(HALF,EnumHalf.TOP);
+        }
+        switch(meta%4){
+            case 0: state = state.withProperty(FACING,EnumFacing.EAST);
+                break;
+            case 1: state = state.withProperty(FACING,EnumFacing.WEST);
+                break;
+            case 2: state = state.withProperty(FACING,EnumFacing.SOUTH);
+                break;
+            case 3: state = state.withProperty(FACING,EnumFacing.NORTH);
+                break;
+        }
+        return state;
     }
 
     @Override
     public int getMetaFromState(IBlockState state) {
-       return 0;
+       int par = state.getValue(HALF).equals(EnumHalf.BOTTOM)?0:4;
+       int meta;
+       switch((EnumFacing)state.getValue(FACING)){
+           case NORTH:
+               meta = 3;
+               break;
+           case SOUTH:
+               meta = 2;
+               break;
+           case WEST:
+               meta = 1;
+               break;
+           case EAST:
+               meta = 0;
+               break;
+           default:
+              return -1;
+       }
+       return par+meta;
     }
+
+
 
     @Override
     public int damageDropped(IBlockState state) {
