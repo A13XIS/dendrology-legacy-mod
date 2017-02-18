e5006fab1eced15bf0df547449735e81daf0f06e
A13XIS <A13XIS@users.noreply.github.com>
2017-02-18 22:17:23 +0100

    Fix Server Crash completely

--- src/main/java/inc/a13xis/legacy/dendrology/proxy/CommonProxy.java
+++ src/main/java/inc/a13xis/legacy/dendrology/proxy/CommonProxy.java
@@ -4,13 +4,26 @@ package inc.a13xis.legacy.dendrology.proxy;
 import inc.a13xis.legacy.dendrology.TheMod;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
 
 public class CommonProxy {
     public void registerRenders(){}
 
-    public void onItemRightClick(ItemStack itemStack, World world, EntityPlayer player){}
+    public void onItemRightClick(ItemStack content, World world, EntityPlayer player){
+        final String message;
+        if (content == null)
+            message = TheMod.fallBackExsists()?TheMod.getFallBack().formatAndSafeServerTranslate(null,TheMod.MOD_ID+":parcel.empty"): I18n.translateToFallback(TheMod.MOD_ID+":parcel.empty");
+        else
+        {
+            final String itemName = TheMod.fallBackExsists()?TheMod.getFallBack().formatAndSafeServerTranslate(null,content.getItem().getUnlocalizedName(content) + ".name"):I18n.translateToFallback(content.getItem().getUnlocalizedName(content) + ".name");
+            message = TheMod.fallBackExsists()?TheMod.getFallBack().formatAndSafeServerTranslate(null,TheMod.MOD_ID+":parcel.full",itemName):I18n.translateToFallback(TheMod.MOD_ID+":parcel.full");
+
+        }
+
+        player.addChatMessage(new TextComponentString(message));
+    }
 
 
     public String safeTranslate(String settingName) {
